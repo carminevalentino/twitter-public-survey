e6b342a3c1db08f9830c7a02c530fb2513c1fe00
Carmine Valentino <cvalentino3@gmail.com>
2013-06-04 16:39:10 -0300

    Update tweetCloud.R

--- tweetCloud.R
+++ tweetCloud.R
@@ -12,6 +12,6 @@ tweetCorpus <- tm_map(tweetCorpus, removePunctuation)
 tweetCorpus <- tm_map(tweetCorpus, tolower)
 tweetCorpus <- tm_map(tweetCorpus, function(x) removeWords(x, stopwords("english")))
 
-png("tweetCloud3.png", width=1280,height=800)
-wordcloud(tweetCorpus, max.words=350, vfont=c("sans serif","plain"), min.freq=3, scale=c(25,4), colors=brewer.pal(3, "Spectral"))
+png("tweetCloud.png", width=12,height=8, units='in', res=300)
+wordcloud(tweetCorpus, max.words=350, rot.per=.15, vfont=c("sans serif","plain"), min.freq=3, scale=c(45,.8), colors=brewer.pal(3, "Dark2"))
 dev.off()
